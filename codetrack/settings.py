6a2e7236ca3def7c61920dd5005aa7a52814a0fb
Sumit Singh <76198431+abcsumits@users.noreply.github.com>
2023-09-15 04:31:13 +0530

    Update settings.py

--- codetrack/settings.py
+++ codetrack/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
