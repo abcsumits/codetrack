6f921586da307c285f8b0c39b3af84eb7ed30380
Sumit Singh <76198431+abcsumits@users.noreply.github.com>
2023-09-15 15:50:00 +0530

    Update settings.py

--- codetrack/settings.py
+++ codetrack/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
